711fe8e4f133c823bdc8560d443fa2b6fb3fd3e3
Victor Rocha <vrsarmento@gmail.com>
2020-04-01 20:31:56 -0300

    Template-dev.html

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = validate({
     new ExtractTextplugin('[name]-[hash].css'),
     new HtmlPlugin({
       title: 'React Boilerplate App',
-      template: path.join(__dirname, 'src', 'html', 'template.html')
+      template: path.join(__dirname, 'src', 'html', 'template-dev.html')
     })
   ],
 
