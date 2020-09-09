379a031e3bd841ee1883f215d2272a78fde58d29
Victor Rocha <vrsarmento@gmail.com>
2020-09-09 19:48:48 -0300

    Reseting fields after save

--- src/components/register-video.js
+++ src/components/register-video.js
@@ -24,13 +24,17 @@ const Form = styled.form`
 `
 
 const mapDispatchToProps = (dispatch) => ({
-  onSubmit: (e) => {
+  onSubmit: async (e) => {
     e.preventDefault()
+    e.persist() // avoiding React to set event to 'null' after usage
+
     const {
       id: { value: id },
       title: { value: title }
     } = e.target
-    dispatch(registerVideo({ id, title }))
+    await dispatch(registerVideo({ id, title }))
+    e.target.reset()
+    e.target[0].focus()
   }
 })
 
