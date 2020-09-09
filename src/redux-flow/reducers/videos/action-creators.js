1d7c64f42e1686b10757c1745636f7d0a4fb3be7
Victor Rocha <vrsarmento@gmail.com>
2020-09-09 19:29:22 -0300

    Reducer Videos

---
+++ src/redux-flow/reducers/videos/action-creators.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import { ADD_VIDEO } from './actions'
+import { db } from 'config/firebase'
+
+export const registerVideo = ({ id, title }) => async (dispatch) => {
+  await db.ref('videos').child(id).update({ id, title })
+  dispatch(addVideo({ id, title }))
+}
+
+export const addVideo = ({ id, title }) => ({
+  type: ADD_VIDEO,
+  payload: { id, title }
+})
---
+++ src/redux-flow/reducers/videos/videos.test.js
@@ -0,0 +1,3 @@
+'use strict'
+
+import { expect } from 'chai'
